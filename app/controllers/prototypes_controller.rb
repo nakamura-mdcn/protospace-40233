36e952bdff5c8c09af5e2046108ee7b02268b3af
Kalkan <gjtqy985@gmail.com>
2024-01-12 00:10:39 +0900

    4つ目の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :move_to_index, except: [:index, :show]
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -27,6 +26,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    if @prototype.user_id != current_user.id
+      redirect_to root_path
+    end
   end
 
   def update
@@ -49,7 +51,10 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end

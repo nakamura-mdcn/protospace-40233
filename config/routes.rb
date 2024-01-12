159c8466afdb865ba288080dcccd87827829799f
Kalkan <gjtqy985@gmail.com>
2024-01-12 16:08:42 +0900

    復習で手直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  
   resources :users, only: :show
   resources :prototypes do
     resources :comments, only: :create
